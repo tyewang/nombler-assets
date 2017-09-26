74378e068b91e5b57b6315983d1e2e06002fabad
Tye Wang <tyewang@outlook.com>
2017-09-26 17:44:35 -0400

    Add pull-center-vertical

--- js/nombler.js
+++ js/nombler.js
@@ -4,4 +4,9 @@
       $(this).css('margin-top', ($(this).parent().height()-$(this).height()));
     });
   });
+  $(document).ready(function(){
+    $('.pull-center-vertical').each(function() {
+      $(this).css('margin-top', ($(this).parent().height()-$(this).height())/2);
+    });
+  });
 }($));
