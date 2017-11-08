cf3d2a73525f56c49e7253266d05d67ccee5fd36
Tye Wang <tyewang@outlook.com>
2017-11-08 16:49:18 -0500

    Connect form to google maps

--- js/get_notified.js
+++ js/get_notified.js
@@ -5,17 +5,24 @@
         $('.submit-button').removeClass('disabled');
         $('.submit-button').prop('disabled', false);
         $('.submit-button').val('Submit');
+        $('.submit-button').focus();
       }
     });
     $('form').submit(function(e){
       $('.submit-button').val('');
       $('.submit-button').addClass('submitting');
-      setTimeout(function() {
-        $('.submit-button').removeClass('submitting');
-        $('.submit-button').addClass('submitted');
-        $('.submit-button').prop('disabled', true);
-        $('.submit-button').val("Thanks! We'll be in touch :)");
-      }, 2000);
+      $.ajax({
+        type: 'POST',
+        url: 'https://docs.google.com/forms/d/e/1FAIpQLSd1ReAyzkNIn0jfJxVUZ7vNT1sA5yfVNl0BmmHJavaW4IxRog/formResponse',
+        data: $('form').serialize(),
+        complete: function () {
+          // we don't care about error or success since we're blocked from viewing the response by CORS
+          $('.submit-button').removeClass('submitting');
+          $('.submit-button').addClass('submitted');
+          $('.submit-button').prop('disabled', true);
+          $('.submit-button').val("Thanks! We'll be in touch :)");
+        }
+      });
       e.preventDefault();
     });
   });
