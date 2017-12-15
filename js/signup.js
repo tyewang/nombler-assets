e3b498fc56cd33b38090f8b67a7f3cf50135e78b
Tye Wang <tyewang@outlook.com>
2017-12-15 12:34:01 -0500

    Test

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,26 @@
 (function ($) {
+  var setupGoogleLogin = function() {
+    gapi.load('auth2', function(){
+      auth2 = gapi.auth2.init({
+        client_id: '954394665198-fnkvnsjg2ad94rdakagv9qfsp45r8j9j.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+      });
+      auth2.attachClickHandler(
+        document.getElementById('google-social-login'),
+        {},
+        function(googleUser) {
+          console.log("Signed in: " + googleUser.getBasicProfile().getName());
+        },
+        function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        }
+      );
+    });
+  };
+
+
   $(document).ready(function(){
+    setupGoogleLogin();
     $('input').on('keypress', function() {
       var requiredFields = [
         '#name',
